3ff873aca05e39a3b037b938cb0ae55211b9b14a
susmitha-vaddella <82006156+susmitha-vaddella@users.noreply.github.com>
2021-04-07 20:33:52 -0400

    Update config.php

--- simple/config.php
+++ simple/config.php
@@ -1,10 +1,9 @@
 <?php
 /* Database credentials. */
-define('DB_SERVER', "us-cdbr-east-03.cleardb.com");
-define('DB_USERNAME', "b11c1dbedee6de");
-define('DB_PASSWORD', "83571e6c");
-define('DB_NAME', "heroku_5abecf7d5ddee5f");
- 
+define('DB_SERVER', 'us-cdbr-east-03.cleardb.com');
+define('DB_USERNAME', 'b78d2002c29757');
+define('DB_PASSWORD', 'e72f0758');
+define('DB_NAME', 'heroku_fb60028f0ab10e6')
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
